cfcddcc65f6d859fc683bdafbe5915be89fa9967
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-03-29 06:43:55 +0100

    Login page object model,test login with valid credentials

--- src/main/java/pages/Login.java
+++ src/main/java/pages/Login.java
@@ -1,2 +1,40 @@
-package pages;public class Login {
+package pages;
+
+import drivers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.IOException;
+
+public class Login {
+    WebDriver driver;
+    public Login() throws IOException {
+        driver =  DriverManager.getInstance().getDriver();
+        PageFactory.initElements(driver,this);
+    }
+    // Define Page elements
+    @FindBy(css = "#content > div > div > div > div > div.card-header")
+    WebElement loginHeader;
+    @FindBy(css = "#form-login label[for='input-username']")
+    WebElement usernameLabel;
+    @FindBy(id = "input-username")
+    WebElement username;
+    @FindBy(css = "#form-login label[for='input-password']")
+    WebElement passwordLabel;
+    @FindBy(id = "input-password")
+    WebElement password;
+    @FindBy(linkText = "Forgotten Password")
+    WebElement forgottenPassword;
+    @FindBy(css = "#form-login button[type='submit']")
+    WebElement submitButton;
+    @FindBy(id="alert")
+    WebElement alert;
+
+    public void login_user(String username, String password){
+        this.username.sendKeys(username);
+        this.password.sendKeys(password);
+        submitButton.click();
+    }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,25 +1,33 @@
 import drivers.DriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
+import pages.Login;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class LoginTest {
     WebDriver driver;
     DriverManager driverManager;
+    Login login;
     @BeforeTest
-    public void setDriverManager() throws IOException {
+    public void set_driver_manager() throws IOException {
+        login = new Login();
         driverManager = DriverManager.getInstance();
         driver = driverManager.getDriver();
         driver.get("https://demo.opencart.com/admin/index.php");
-
     }
     @Test
-    public void loginWithValidCredentials(){
-        System.out.println("Hello America");
+    public void login_with_valid_credentials(){
+        login.login_user("demo", "demo");
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.urlContains("dashboard"));
+        assert driver.getCurrentUrl().contains("dashboard");
     }
     @AfterTest
-    public void closeDriverManager(){
+    public void close_driver_manager(){
         driverManager.quitDriverInstance();
     }
 }
